d7caa2a98362f343f0eda7475ed21f2cda173efd
Luisan <luisangelsc@gmail.com>
2020-02-07 17:08:54 -0600

    allowed CORS origin as *

--- server.js
+++ server.js
@@ -18,50 +18,18 @@ app.use(function(req, res, next) {
     "Access-Control-Allow-Headers",
     "Origin, X-Requested-With, Content-Type, Accept"
   );
-  // console.log("REQ KEYS:", Object.keys(res.req));
-  // console.log("REQ params:", Object.keys(res.req.params));
-  // console.log("REQ query:", Object.keys(res.req.query));
-  // console.log("REQ res:", Object.keys(res));
-  // console.log("HEADERS:", req.headers);
-  // console.log("KEYS:", Object.keys(req.headers));
-  // console.log("signed cookies:", res.signedCookies);
-
   next();
 });
 
 app.get("/", function(req, res) {
   // Cookies that have not been signed
   console.log("Cookies: ", req.cookies);
-
   // // Cookies that have been signed
   console.log("Signed Cookies: ", req.signedCookies);
   return res.json("Up and running!");
 });
-const router = express.Router();
-
-async function getTokenForServer(req) {
-  console.log("For Server:", req);
-  if (req.headers.cookie) {
-    const jwtFromCookie = req.headers.cookie
-      .split(";")
-      .find(c => c.trim().startsWith("jwtToken="));
-    if (!jwtFromCookie) {
-      return undefined;
-    }
-    const token = jwtFromCookie.split("=")[1];
-    const validToken = await verifyToken(token);
-    if (validToken) {
-      console.log(jwt.decode(token));
-      return;
-    } else {
-      return undefined;
-    }
-  }
-}
-// getTokenForServer();
 
 // // this is our MongoDB database
-//WE WILL TRY REMOVING ${COLLECTION} AND SETTING THE COLLECTIONON SCHEMAS
 const dbRoute = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.CLUSTER}/images?retryWrites=true&w=majority`;
 // // connects our back end code with the database
 mongoose.connect(dbRoute, {
@@ -71,8 +39,6 @@ mongoose.connect(dbRoute, {
 });
 
 let db = mongoose.connection;
-
-console.log("dbRoute:", dbRoute);
 
 db.once("open", () => console.log("connected to the database"));
 
@@ -85,27 +51,19 @@ app.use(bodyParser.json());
 //  GET #####
 app.get("/getImageUrl", (req, res) => {
   const data = req.query;
-  // console.log("GET:", req.query);
-  Data.find(data, (err, data) => {
-    if (err) return res.json({ success: false, error: err });
-    // console.log(data)
-    return res.json({ success: true, data: data });
-  });
+  if (Data) {
+    Data.find(data, (err, data) => {
+      if (err) return res.json({ success: false, error: err });
+      return res.json({ success: true, data: data });
+    });
+  }
 });
 
 app.post("/setCollection", (req, res) => {
-  console.log("APOOOOOST");
-  // console.log(req.body);
-  // console.log(Object.keys(req.body));
+  console.log("Setting Collection");
   const collection = /.*@/.exec(req.body.email)[0].slice(0, -1);
-  console.log("colletiON:", collection);
-  // module.exports = mongoose.model("Data", DataSchema);
+  console.log("collection:", collection);
   Data = mongoose.model(collection, DataSchema);
-  // const col = Data.db("test").collection("newCollection");
-  // col.insert({ name: req.body.nickname });
-  console.log("dbRoute:", dbRoute);
-
-  // console.log("Setting colec:", req.body);
 });
 
 //  POST #####
@@ -144,27 +102,9 @@ app.put("/updateData", (req, res) => {
     doc.imageURL = imageURL ? imageURL : doc.imageURL;
     doc.save();
   }
+
   getDoc();
 
-  // Data.find(query, null, (res) => {
-  //   // res.send("Ninjas are in");
-  //   // console.log("DATA:", data);
-  //   console.log("res:", res );
-  // })
-
-  // Data.find(query)
-  //   .then(response => console.log("RESPONSE:", response));
-  // console.log(doc, '\n\n');
-  // console.log(doc.schema, '\n\n');
-  // console.log(typeof doc);
-
-  // doc.pegName = pegName;
-  // doc.save()
-  // Data.findOneAndUpdate(query, data, res => {console.log("updated or at least ran:", res); return res})
-  // Data.find(id, update, (err) => {
-  //   if (err) return res.json({ success: false, error: err });
-  //   return res.json({ success: true });
-  // });
   return res.json("hello friend");
 });
 
@@ -176,6 +116,8 @@ app.delete("/deleteData", (req, res) => {
     return res.json({ success: true });
   });
 });
+
+const router = express.Router();
 
 app.use("/", router);
 
